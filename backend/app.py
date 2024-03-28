209008c6e35aefe038c1e73ab44b6d2cbed772f6
nikita <nikitapatil12012003@gmail.com>
2024-03-28 18:11:00 +0530

    This backend is changed

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/api/request', methods=['GET'])
 def send_request():
     # You can add any necessary logic here
-    return '200 OK'
+    return 'Response is got from backend!Success'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
